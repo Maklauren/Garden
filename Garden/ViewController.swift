0848300feb16b3d29a70cf9d4b8b55731e5239af
Pavel Chernook <Maklauren97@gmail.com>
2022-08-03 18:08:01 +0300

    Implemented getting information about a flower from Wikipedia

--- Garden/ViewController.swift
+++ Garden/ViewController.swift
@@ -8,15 +8,24 @@
 import UIKit
 import CoreML
 import Vision
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
-    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var flowerDescriptionLabel: UILabel!
+    
+    private let imagePicker = UIImagePickerController()
+    
+    private let wikipediaURL = "https://en.wikipedia.org/w/api.php"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Garden"
         
         imagePicker.delegate = self
         imagePicker.allowsEditing = true
@@ -30,8 +39,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             
             detect(image: convertedCIImage)
-            
-            imageView.image = userPickedImage
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
@@ -48,6 +55,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
 
             self.navigationItem.title = classification.identifier.capitalized
+            
+            self.requestInfo(flowerName: classification.identifier)
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
@@ -56,6 +65,35 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             try handler.perform([request])
         } catch {
             print(error)
+        }
+    }
+    
+    func requestInfo(flowerName: String) {
+        let parameters : [String:String] = [
+          "format" : "json",
+          "action" : "query",
+          "prop" : "extracts|pageimages",
+          "exintro" : "",
+          "explaintext" : "",
+          "titles" : flowerName,
+          "indexpageids" : "",
+          "redirects" : "1",
+          "pithumbsize" : "500"]
+        
+        AF.request(wikipediaURL, method: .get, parameters: parameters).responseJSON { response in
+            switch response.result {
+            case .success(let resultValue):
+                let flowerJSON: JSON = JSON(resultValue)
+                
+                let pageID = flowerJSON["query"]["pageids"][0].stringValue
+                let flowerDescription = flowerJSON["query"]["pages"][pageID]["extract"].string
+                let flowerImageURL = flowerJSON["query"]["pages"][pageID]["thumbnail"]["source"].stringValue
+                
+                self.imageView.sd_setImage(with: URL(string: flowerImageURL))
+                self.flowerDescriptionLabel.text = flowerDescription
+            case .failure(let error):
+                print(error)
+            }
         }
     }
     
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Garden' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Garden
+
+pod 'SwiftyJSON'
+pod 'Alamofire'
+pod 'SDWebImage'
+
+end
